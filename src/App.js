aacd6d4a662586e85ff59bd4f550f7288207d0f8
hasan-alkhalaf <eng.hasanalkhalaf92@gmail.com>
2021-09-11 15:12:48 +0300

    added some editing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+
+import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="todo-app">
-     <TodoList />
+    <div className='todo-app'>
+      <TodoList />
     </div>
   );
 }
